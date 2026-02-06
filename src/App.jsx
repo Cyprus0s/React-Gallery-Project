c38039b89c28ebce4f368466e2f5363691050e44
Krishna Gupta <brotherzzboth@gmail.com>
2026-02-07 00:38:54 +0530

    updating: gallery text

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     getdata()
   }, [index])
 
-  let printUserName = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>No User Available</h3>
+  let printUserName = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading...</h3>
 
   if (userData.length > 0) {
     printUserName = userData.map((elem, idx) => {
